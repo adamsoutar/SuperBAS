ed70443620e77b369c70effc09b7195a77464ce0
Adam <adam@overflo.me>
2020-01-26 15:41:36 +0000

    Update build script for .NET Core 3.1

--- buildAll.sh
+++ buildAll.sh
@@ -2,16 +2,16 @@ cd SuperBAS
 
 # Mac
 dotnet publish -c Release --self-contained -r osx-x64 /p:PublishSingleFile=true
-cp bin/Release/netcoreapp3.0/osx-x64/publish/SuperBAS ../releases/SuperBAS-macOS-x64
+cp bin/Release/netcoreapp3.1/osx-x64/publish/SuperBAS ../releases/SuperBAS-macOS-x64
 
 # Linux
 dotnet publish -c Release --self-contained -r linux-x64 /p:PublishSingleFile=true
-cp bin/Release/netcoreapp3.0/linux-x64/publish/SuperBAS ../releases/SuperBAS-linux-x64
+cp bin/Release/netcoreapp3.1/linux-x64/publish/SuperBAS ../releases/SuperBAS-linux-x64
 
 # Windows
 # 64
 dotnet publish -c Release --self-contained -r win-x64 /p:PublishSingleFile=true
-cp bin/Release/netcoreapp3.0/win-x64/publish/SuperBAS.exe ../releases/SuperBAS-x64.exe
+cp bin/Release/netcoreapp3.1/win-x64/publish/SuperBAS.exe ../releases/SuperBAS-x64.exe
 # 32
 dotnet publish -c Release --self-contained -r win-x86 /p:PublishSingleFile=true
-cp bin/Release/netcoreapp3.0/win-x86/publish/SuperBAS.exe ../releases/SuperBAS-x86.exe
+cp bin/Release/netcoreapp3.1/win-x86/publish/SuperBAS.exe ../releases/SuperBAS-x86.exe
