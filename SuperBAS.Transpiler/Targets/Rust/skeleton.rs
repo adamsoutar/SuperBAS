ab22c7e41c07cde44237b7a2601eda2c24f19e4f
Adam Soutar <adam@overflo.me>
2021-11-16 13:22:39 +0000

    Begin to implement a Rust compiler target This will enable SuperBAS at 100% native speeds!

---
+++ SuperBAS.Transpiler/Targets/Rust/skeleton.rs
@@ -0,0 +1,26 @@
+#[derive(Default)]
+pub struct SuperBASProgram {
+    /*DECLARATIONS*/
+}
+
+impl SuperBASProgram {
+    fn go_sub(&mut self, raw_line: f64) {
+        let mut line = raw_line;
+        loop {
+            match line {
+                -1.0 => return,
+                /*BODY*/
+                _ => panic!("Can't GOTO {} - invalid line number.", line)
+            }
+        }
+    }
+
+    fn execute (&mut self) {
+        self.go_sub(/*LOWESTLINE*/f64)
+    }
+}
+
+fn main() {
+    let mut program = SuperBASProgram::default();
+    program.execute();
+}
